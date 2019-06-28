8e3c30916b96698ff3b76abd9d0e64ddba6ee3d7
Emre Özkan <emreozkan@windowslive.com>
2019-06-28 23:41:33 +0300

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main() fmt.Println("hello world")
+func main() {
+	fmt.Println("hello world")
+
+	foo()
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("hello foo")
 
 }
